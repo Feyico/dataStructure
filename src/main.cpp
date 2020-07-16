e228f6d33a70097b09e987db42fff48e7716281a
Feyico <fycqookuer@foxmail.com>
2020-07-16 19:33:44 +0800

    A：新的测试代码

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "Common.h"
 #include "HeapStack.h"
 #include "DoubleStack.h"
+#include "LinkStack.h"
 
 #define STACKSIZE 10
 
@@ -92,9 +93,86 @@ void OperateDoubleStack()
     return;
 }
 
+void Menu()
+{
+    cout<<"+===========================================+"<<endl;
+    cout<<"|    1.测试元素进栈      2.测试元素出栈         |"<<endl;
+    cout<<"+===========================================+"<<endl;
+    cout<<"|    3.得到栈顶元素      4.判断栈是否为空       |"<<endl;
+    cout<<"+===========================================+"<<endl;
+    cout<<"|    5.打印栈元素        0.退出               |"<<endl;
+    cout<<"+===========================================+"<<endl;
+}
+
+void OperateLinkStack()
+{
+    LinkStack<int> lStackMember;
+
+    for(int i=0;i<10;i++)
+    {
+        lStackMember.Push(i+1);
+    }
+    Menu();
+    int choose=1;
+    int tmp;
+    while(choose)
+    {
+        cout<<"请输入你的选择："<<endl;
+        cin>>choose;
+        switch (choose) 
+        {
+        
+            case 1:
+                {
+                    cout<<"请输入你要进栈的元素：";
+                    cin>>tmp;
+                    lStackMember.Push(tmp);
+                }
+                break;
+            case 2:
+                {
+                    int x;
+                    lStackMember.Pop(x);
+                    cout<<"出栈的元素为："<<x<<endl;
+                }
+                break;
+            // case 3:
+            //     {
+            //         int x1;
+            //         lStackMember.getTop(x1);
+            //         cout<<"栈顶元素为："<<x1<<endl;
+            //     }
+            //     break;
+            // case 4:
+            //     if(lStackMember.isEmpty() == true){
+            //         cout<<"该栈为空！"<<endl;
+            //     }
+            //     else{
+            //         cout<<"栈不为空！"<<endl;
+            //     }
+            //     break;
+            case 5:
+                cout<<"栈的元素为:"<<endl;
+                lStackMember.printStackData();
+                break;
+            case 0:
+                return;
+                break;
+            default:
+                break;
+            cout << endl;
+            Menu();
+        }
+        cout<<endl;
+        Menu();
+    }
+    return;
+}
+
 int main()
 {
-    OperateStack();
-    OperateDoubleStack();
+    //OperateStack();
+    //OperateDoubleStack();
+    OperateLinkStack();
     return 0;
 }
